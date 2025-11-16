7a1b32ce1263932f6fea82b968d850f4e739495b
Lavanyaa E <115788511+lavanyaa0723@users.noreply.github.com>
2025-11-16 16:54:39 +0530

    Update SQL Coding challenge 1.sql

--- SQL Coding challenge 1.sql
+++ SQL Coding challenge 1.sql
@@ -1,8 +1,10 @@
 -- SQL Coding Challenges Day 1--
+-- DDL Commands --
 
 CREATE DATABASE Hospital;
 USE Hospital;
 
+-- Create Table --
 CREATE TABLE Patients(
 Patient_ID int,
 Patient_Name varchar(50),
@@ -12,14 +14,18 @@ AdmissionDate date
 );
 select*from Patients;
 
+-- Add Column --
 ALTER TABLE Patients ADD COLUMN DoctorAssigned varchar(50);
 select*from Patients;
 
+-- Modify Column --
 ALTER TABLE Patients MODIFY COLUMN Patient_Name varchar(100);
- 
+
+-- Rename Table --
  RENAME TABLE Patients to Patient_Info;
  select*from Patient_Info;
- 
+
+-- Truncate vs Drop --
  TRUNCATE TABLE Patient_Info;
  DROP TABLE Patient_Info;
 
